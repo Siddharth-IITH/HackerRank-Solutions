c41c76eb13b075705622098053029629a12f68b6
Siddharth-IITH <36755133+Siddharth-IITH@users.noreply.github.com>
2020-08-01 00:42:50 +0530

    Update Java Anagrams.java

--- Java Anagrams.java
+++ Java Anagrams.java
@@ -4,25 +4,31 @@ public class Solution {
 
     static boolean isAnagram(String a, String b) {
 
-        
+        //Create a HashMap to store frequency of characters
         java.util.HashMap<Character, Integer> map= new java.util.HashMap<>();
 
+        //Add character frequency from string 'a'
         for (Character c : a.toCharArray()){
             c=java.lang.Character.toLowerCase(c);
            int count = map.containsKey(c) ? map.get(c) : 0;
             map.put(c, count + 1);
             }
+        
+        //subtract character frequency from map using string 'b'
         for (Character c : b.toCharArray()){
             c=java.lang.Character.toLowerCase(c);
            int count = map.containsKey(c) ? map.get(c) : 0;
             map.put(c, count - 1);
             }
+        
+        //check if every frequency is zero which means every character has equal freq in both strings
         for ( int i:map.values()){
             if (i!=0){
                 return false;
             }
         }
-return true;
+        
+    return true;
     }
 
     public static void main(String[] args) {
