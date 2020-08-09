f8573028f8ba8440b5ed257d8774ec47328c52e8
Siddharth-IITH <36755133+Siddharth-IITH@users.noreply.github.com>
2020-08-09 14:06:15 +0530

    Create height of tree.py

---
+++ height of tree.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+    lH=0
+    rH=0
+
+    if root==None:
+        return -1
+    if not root.left and not root.right:
+        return 0
+
+    lH=height(root.left)
+    rH=height(root.right)
+
+    return  max(lH,rH)+1
+    
